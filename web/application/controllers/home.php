06611efd9c42d849e08efd125e022f2ef9402f24
Aaron Collegeman <aaron@collegeman.net>
2011-01-18 22:10:57 -0500

    Discovered 'default' can't be a class name. Changed it to 'home'

---
+++ web/application/controllers/home.php
@@ -0,0 +1,15 @@
+<?php
+
+class Home extends Controller {
+
+	function Home() {
+		parent::Controller();	
+	}
+	
+	function index() {
+		$this->load->view('home/index');
+	}
+}
+
+/* End of file home.php */
+/* Location: ./system/application/controllers/home.php */
