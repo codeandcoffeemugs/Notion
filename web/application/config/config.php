58061e401745ac6c33a28f539b877d1d99c6c5ca
Aaron Collegeman <aaron@collegeman.net>
2011-01-29 21:05:07 -0500

    Added my_sessions table to the mysql.sql file, and updated the AUTO_INCREMENT setting for the my_options table In config.php, set CI sessions table name to "my_sessions" and set default session timeout to 30 minutes

--- web/application/config/config.php
+++ web/application/config/config.php
@@ -228,15 +228,15 @@ $config['encryption_key'] = "";
 | 'session_cookie_name' = the name you want for the cookie
 | 'encrypt_sess_cookie' = TRUE/FALSE (boolean).  Whether to encrypt the cookie
 | 'session_expiration'  = the number of SECONDS you want the session to last.
-|  by default sessions last 7200 seconds (two hours).  Set to zero for no expiration.
+|  by default sessions last 1800 seconds (30 minutes).  Set to zero for no expiration.
 | 'time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_session';
-$config['sess_expiration']		= 7200;
+$config['sess_cookie_name']		= 'idea_session';
+$config['sess_expiration']		= 1800;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= FALSE;
-$config['sess_table_name']		= 'ci_sessions';
+$config['sess_table_name']		= 'my_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update'] 	= 300;
